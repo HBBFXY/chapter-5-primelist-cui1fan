572b7fd1f93a265e8b39d83eae2b199ba4c1b772
cui1fan <1819285039@qq.com>
2025-10-20 17:38:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ def PrimeList(N):
     参数:    N - 正整数    
     返回:    str - 包含所有小于 N 的质数的字符串，空格分隔
     """
-    def PrimeList(N):
-    """
     primes = []
     for num in range(2, N):
         is_prime = True
